331e8a807263dd2bfc222c9d599cd17db71532ec
Josh Pelham <jshplhm@gmail.com>
2026-05-05 18:05:05 -0700

    trying to automate my photos json

---
+++ generate_manifest.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import json, os
+
+manifest = {}
+for year in ['2021','2022','2023','2024','2025','2026','xo']:
+    manifest[year] = []
+    folder = f'photos/{year}'
+    if not os.path.exists(folder):
+        continue
+    for f in sorted(os.listdir(folder)):
+        if f.lower().endswith(('.jpg','.jpeg','.png')):
+            with Image.open(f'{folder}/{f}') as img:
+                w, h = img.size
+                manifest[year].append({'filename': f, 'w': w, 'h': h})
+
+with open('photos.json', 'w') as out:
+    json.dump(manifest, out)
+
+print('Done')
